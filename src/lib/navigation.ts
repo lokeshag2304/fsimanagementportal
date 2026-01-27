01b18e88bfddf4644ab4a6dbec1eb9c23e8351bc
username <email>
2026-01-27 13:21:59 +0530

    git push

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -1,16 +1,21 @@
 export const navigationTabs = [
   { name: "Dashboard", href: "/" },
   { name: "User Management", href: "/SuperAdmin/user-management", hasDropdown: true, submenu: [
-    { name: "Support Admin", href: "/user-management/support-admin" },
-    { name: "Customer Admin", href: "/user-management/customer-admin" }
+    { name: "Client", href: "/SuperAdmin/clients" },
+    { name: "User Admin", href: "/SuperAdmin/user" }
   ] },
     { name: "Products", href: "/SuperAdmin/products" },
   { name: "Domain", href: "/SuperAdmin/domain" },
-  { name: "Courses", href: "/SuperAdmin/courses", hasDropdown: true },
-  { name: "Bootcamp", href: "/SuperAdmin/bootcamp", hasDropdown: true },
-  { name: "Team Training", href: "/SuperAdmin/team-training", hasDropdown: true },
-  { name: "EBook", href: "/SuperAdmin/ebooks", hasDropdown: true },
-  { name: "Enrollments", href: "/SuperAdmin/enrollments", hasDropdown: true },
-  { name: "Reports", href: "/SuperAdmin/reports", hasDropdown: true },
-  { name: "Affiliate", href: "/SuperAdmin/affiliate" },
+  { name: "Domains", href: "/SuperAdmin/domains" },
+  { name: "Counter", href: "/SuperAdmin/counter", hasDropdown: false },
+  { name: "Emails", href: "/SuperAdmin/sub-email", hasDropdown: false },
+  { name: "Hosting", href: "/SuperAdmin/hosting", hasDropdown: false },
+  { name: "Search Result", href: "/SuperAdmin/search-result", hasDropdown: false },
+  { name: "SSL", href: "/SuperAdmin/ssl", hasDropdown: false },
+  { name: "Subcription", href: "/SuperAdmin/subcription", hasDropdown: false },
+  // { name: "Team Training", href: "/SuperAdmin/team-training", hasDropdown: true },
+  // { name: "EBook", href: "/SuperAdmin/ebooks", hasDropdown: true },
+  // { name: "Enrollments", href: "/SuperAdmin/enrollments", hasDropdown: true },
+  // { name: "Reports", href: "/SuperAdmin/reports", hasDropdown: true },
+  // { name: "Affiliate", href: "/SuperAdmin/affiliate" },
 ]
